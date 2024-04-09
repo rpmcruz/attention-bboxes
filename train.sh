73c5ca9c63ad652237096afbc20f934884cd16a2
Ricardo Cruz <rpcruz@fe.up.pt>
2024-04-09 19:00:36 +0100

    Forgot to add output name...

--- train.sh
+++ train.sh
@@ -16,12 +16,12 @@ for PENALTY2 in $PENALTIES; do
     OUT="model-$DATASET-GaussianGrid-penalty-$PENALTY1-$PENALTY2-usesoftmax-0.pth"
     if [ ! -f $OUT ]; then
         echo $OUT
-        python3 train.py $DATASET GaussianGrid --epochs $EPOCHS --penalty1 $PENALTY1 --penalty2 $PENALTY2 &
+        python3 train.py $DATASET GaussianGrid $OUT --epochs $EPOCHS --penalty1 $PENALTY1 --penalty2 $PENALTY2 &
     fi
     OUT="model-$DATASET-GaussianGrid-penalty-$PENALTY1-$PENALTY2-usesoftmax-1.pth"
     if [ ! -f $OUT ]; then
         echo $OUT
-        python3 train.py $DATASET GaussianGrid --epochs $EPOCHS --penalty1 $PENALTY1 --penalty2 $PENALTY2 --use-softmax &
+        python3 train.py $DATASET GaussianGrid $OUT --epochs $EPOCHS --penalty1 $PENALTY1 --penalty2 $PENALTY2 --use-softmax &
     fi
     wait
 done
