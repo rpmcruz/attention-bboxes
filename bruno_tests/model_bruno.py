Improved receptive field. Fixed vanishing gradients

bruno_tests/model_bruno.py
@@ -1,27 +1,28 @@
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
-from utils import * 
+from utils import *
+
 
 class AnchorBackbone(nn.Module):
     def __init__(self) -> list:
         super().__init__()
-        
+
         # What each pixel from an activation maps represents in the image
         self.conv1 = nn.LazyConv2d(128,3,2) # 3x3 original image
         self.conv2 = nn.LazyConv2d(256,3,2) # 7x7 original image
-        self.conv3 = nn.LazyConv2d(512,3,2) # 15x15 original image -> ixstride^levels
+        self.conv3 = nn.LazyConv2d(512,3,2) # 15x15 original image
 
         # Extracting a 1024 representation from different places of the image
-        self.anchor0 = nn.LazyConv2d(1024,3,1) # 17x17 original 
-        self.anchor1 = nn.LazyConv2d(1024,1,1) # 15x15 original 
-        self.anchor2 = nn.LazyConv2d(1024,(3,1),1) # 17x15 original 
-        self.anchor3 = nn.LazyConv2d(1024,(1,3),1) # 15x17 original 
+        self.anchor0 = nn.LazyConv2d(1024,3,1) # 17x17 original
+        self.anchor1 = nn.LazyConv2d(1024,1,1) # 15x15 original
+        self.anchor2 = nn.LazyConv2d(1024,(3,1),1) # 17x15 original
+        self.anchor3 = nn.LazyConv2d(1024,(1,3),1) # 15x17 original
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        x = self.conv3(x)        
+        x = self.conv3(x)
 
         anc0 = self.anchor0(x)
         anc1 = self.anchor1(x)
@@ -29,7 +30,7 @@ class AnchorBackbone(nn.Module):
         anc3 = self.anchor3(x)
 
         return [anc0, anc1, anc2, anc3]
-    
+
 # Attention Head
 class AttentionHead(nn.Module):
     def __init__(self, N: int = 1) -> None:
@@ -42,13 +43,13 @@ class AttentionHead(nn.Module):
         self.V = nn.Linear(in_features=512, out_features=256)
 
         # Defining the activation functions
-        self.act_W1 = nn.ReLU()        
-        self.act_Wa = nn.Sigmoid() # Maybe it is better to change to ReLU
+        self.act_W1 = nn.ReLU()
+        self.act_Wa = nn.ReLU() # Maybe it is better to change to ReLU
         self.act_U = nn.Sigmoid()
-        self.act_V = nn.Tanh()        
+        self.act_V = nn.Tanh()
 
     def forward(self, x: torch.Tensor) -> (torch.Tensor):
-        # Reducing the dimension from 1024 to 512        
+        # Reducing the dimension from 1024 to 512
         h = self.W1(x)
         h = self.act_W1(h)
 
@@ -66,13 +67,13 @@ class AttentionHead(nn.Module):
         att = self.act_Wa(att)
         return att, h
 
-    
+
 class ClusterAttention(nn.Module):
     def __init__(self) -> None:
         super(ClusterAttention, self).__init__()
         self.cluster = nn.LazyConv2d(1,(1,512),(1,512))
         self.act_cluster = nn.Softmax(dim=1)
-    
+
     def forward(self,x):
         x = self.cluster(x[None,...])
         return self.act_cluster(x)
@@ -87,9 +88,9 @@ class AttModel(nn.Module):
 
         self.backbone = AnchorBackbone()
         self.attention_head = AttentionHead()
-        self.attention_cluster = ClusterAttention()        
+        self.attention_cluster = ClusterAttention()
 
-        # placeholders for concatanation 
+        # placeholders for concatanation
         self.rep_cluster = []
         self.att_cluster = []
         self.box_coords = []
@@ -105,5 +106,5 @@ class AttModel(nn.Module):
         final_rep = attention_pooling(feat_clusters, atts=att_clusters.view(-1,1))
 
         logits = self.classifier(final_rep)
-        
-        return self.act_class(logits), att_clusters, proj_clusters
+
+        return logits, att_clusters, proj_clusters

bruno_tests/utils.py
@@ -1,61 +1,65 @@
-import torch
-import numpy as np
-from torch_kmeans import KMeans
 from itertools import compress
 
-# Melhorar essa funcao e entender melhor como é a projeção
-def corner_projection(i: int, j: int, kernel_size: tuple) -> tuple:
-    if kernel_size[0] == 3:
-        w = 17
-        i_proj = i*8
-    else:
-        w = 15
-        i_proj = i*8
+import numpy as np
+import torch
+from torch_kmeans import KMeans
 
-    if kernel_size[1] == 3:
-        h = 17
-        j_proj = j*8
-    else:
-        h = 15
-        j_proj = j*8
+
+# Melhorar essa funcao e entender melhor como é a projeção
+def corner_projection(i: int, j: int, kernel_size: tuple, strides: tuple) -> tuple:
+    w = receptive_field(kernel_size=kernel_size[0], strides=strides)
+    h = receptive_field(kernel_size=kernel_size[1], strides=strides)
+    i_proj = i*np.prod(strides)
+    j_proj = j*np.prod(strides)
 
     return torch.tensor([i_proj, j_proj, w, h], dtype=torch.float32).view(1,-1)
 
+def receptive_field(kernel_size, strides):
+    count = 0
+    for kernel,stride in zip(kernel_size, strides):
+        if not count:
+            img_proj = kernel
+        else:
+            img_proj = kernel + stride*(img_proj-1)
+        count += 1
+    return img_proj
+
 def feat_organizer(features: list) -> list:
-    kernel = [(3,3), (1,1), (3,1), (1,3)]    
+    kernel = [((3,3,3,3), (3,3,3,3)), ((3,3,3,1), (3,3,3,1)), ((3,3,3,3), (3,3,3,1)), ((3,3,3,1), (3,3,3,3))]
+    strides = (2,2,2,1)
     feat_1024 = torch.empty(size=(400,1024))
-    xywh_proj = torch.empty(size=(400,4))            
+    xywh_proj = torch.empty(size=(400,4))
 
     counter = 0
-    for ind in range(len(features)):                
-        anch = features[ind]                
+    for ind in range(len(features)):
+        anch = features[ind]
         x_pos,y_pos = anch.shape[-2],anch.shape[-1]
 
         for i in range(x_pos):
             for j in range(y_pos):
                 feat_1024[counter,:] = anch[:,:,i,j]
-                xywh_proj[counter,:] = corner_projection(i,j, kernel_size=kernel[ind])
+                xywh_proj[counter,:] = corner_projection(i,j, kernel_size=kernel[ind], strides=strides)
                 counter += 1
 
     return feat_1024, xywh_proj
 
 
 def cluster_features(anchor_feat: torch.Tensor, att_feat: torch.Tensor, anchor_proj: torch.Tensor, num_clusters = 10) -> dict:
-    feat_groups = torch.empty(size=(num_clusters, 512))    
-    proj_groups = torch.empty(size=(num_clusters, 4))    
-    
+    feat_groups = torch.empty(size=(num_clusters, 512))
+    proj_groups = torch.empty(size=(num_clusters, 4))
+
     kmeans = KMeans(n_clusters=num_clusters, verbose=False)
     result = kmeans(anchor_feat[None,:,:])
 
     for i in range(num_clusters):
-        aux_tensor = anchor_feat[torch.squeeze(result.labels == i),:]   
-        aux_att = att_feat[torch.squeeze(result.labels == i),:]   
-        aux_proj = anchor_proj[torch.squeeze(result.labels == i),:]   
+        aux_tensor = anchor_feat[torch.squeeze(result.labels == i),:]
+        aux_att = att_feat[torch.squeeze(result.labels == i),:]
+        aux_proj = anchor_proj[torch.squeeze(result.labels == i),:]
 
         feat_groups[i,:] = attention_pooling(tiles_vector=aux_tensor, atts=torch.nn.functional.softmax(aux_att, dim=0))
-        proj_groups[i,:] = attention_pooling(tiles_vector=aux_proj, atts=torch.nn.functional.softmax(aux_att, dim=0)) # Possivel fonte de erro           
-    
+        proj_groups[i,:] = attention_pooling(tiles_vector=aux_proj, atts=torch.nn.functional.softmax(aux_att, dim=0)) # Possivel fonte de erro
+
     return feat_groups, proj_groups
 
-def attention_pooling(tiles_vector: torch.Tensor, atts: torch.Tensor) -> torch.Tensor:    
+def attention_pooling(tiles_vector: torch.Tensor, atts: torch.Tensor) -> torch.Tensor:
     return torch.matmul(torch.t(atts),tiles_vector)