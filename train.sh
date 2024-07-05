bea9c142c22cf180101b159a37b13636211751ff
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2024-07-05 05:20:10 +0100

    Train datasets.

--- train.sh
+++ train.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-DATASET=Birds
+DATASETS="Birds StanfordCars StanfordDogs"
 PENALTIES="0 0.001 0.1 1 10"
 
+for DATASET in $DATASETS; do
 MODEL=OnlyClass
 echo "model-$DATASET-$MODEL"
 python3 train.py model-$DATASET-$MODEL.pth $DATASET $MODEL
 
-MODEL=ProtoPNet
-echo "model-$DATASET-$MODEL"
-python3 train.py model-$DATASET-$MODEL.pth $DATASET $MODEL
+#MODEL=ProtoPNet
+#echo "model-$DATASET-$MODEL"
+#python3 train.py model-$DATASET-$MODEL.pth $DATASET $MODEL
 
 MODEL=Heatmap
 for PENALTY in $PENALTIES; do
-echo "model-$DATASET-$MODEL-l1-$PENALTY-softmax"
-python3 train.py model-$DATASET-$MODEL-l1-$PENALTY-softmax.pth $DATASET $MODEL --penalty-l1 $PENALTY
+#echo "model-$DATASET-$MODEL-l1-$PENALTY-softmax"
+#python3 train.py model-$DATASET-$MODEL-l1-$PENALTY-softmax.pth $DATASET $MODEL --penalty-l1 $PENALTY
 echo "model-$DATASET-$MODEL-l1-$PENALTY-sigmoid"
 python3 train.py model-$DATASET-$MODEL-l1-$PENALTY-sigmoid.pth $DATASET $MODEL --penalty-l1 $PENALTY --sigmoid
 done
 
-MODEL="SimpleDet"
+MODELS="SimpleDet FasterRCNN FCOS DETR"
+for MODEL in $MODELS; do
 for PENALTY in $PENALTIES; do
-echo "model-$DATASET-$MODEL-l1-$PENALTY-softmax"
-python3 train.py model-$DATASET-$MODEL-l1-$PENALTY-softmax.pth $DATASET $MODEL --penalty-l1 $PENALTY
+#echo "model-$DATASET-$MODEL-l1-$PENALTY-softmax"
+#python3 train.py model-$DATASET-$MODEL-l1-$PENALTY-softmax.pth $DATASET $MODEL --penalty-l1 $PENALTY
 echo "model-$DATASET-$MODEL-l1-$PENALTY-sigmoid"
 python3 train.py model-$DATASET-$MODEL-l1-$PENALTY-sigmoid.pth $DATASET $MODEL --penalty-l1 $PENALTY --sigmoid
+done
+done
+
 done
