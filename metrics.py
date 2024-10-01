Fixed some mistakes regarding heatmap normalization.

metrics.py
@@ -79,9 +79,11 @@ class Sparsity(torchmetrics.Metric):
         self.add_state('l1', default=torch.tensor(0.), dist_reduce_fx='sum')
         self.add_state('total', default=torch.tensor(0), dist_reduce_fx='sum')
 
-    def update(self, heatmap):
-        self.l1 += torch.mean(heatmap)
-        self.total += len(heatmap)
+    def update(self, heatmaps):
+        assert len(heatmaps.shape) == 3, f'heatmaps have more than three dimensions: {heatmaps.shape}'
+        heatmaps = heatmaps / heatmaps.amax((1, 2), True)
+        self.l1 += torch.sum(torch.mean(heatmaps, (1, 2)))
+        self.total += len(heatmaps)
 
     def compute(self):
         return self.l1 / self.total

models.py
@@ -266,12 +266,12 @@ class Bboxes2Heatmap(torch.nn.Module):
         yy = torch.arange((1/h)/2, 1, 1/h, device=bboxes.device)
         xx, yy = torch.meshgrid(xx, yy, indexing='xy')
         heatmaps = self.f(xx[None, None], yy[None, None], bboxes[:, 0][..., None, None], bboxes[:, 1][..., None, None], bboxes[:, 2][..., None, None], bboxes[:, 3][..., None, None])
-        heatmaps = heatmaps / (1e-5+torch.amax(heatmaps, 1, True))  # max=1
         if self.use_sigmoid:
             scores = torch.sigmoid(scores)
         else:
             scores = torch.softmax(scores, 1)
         heatmap = torch.sum(scores[..., None, None]*heatmaps, 1)
+        heatmap = heatmap / (1e-5+torch.amax(heatmap, (1, 2), True))  # max=1
         return heatmap
 
 class GaussHeatmap(Bboxes2Heatmap):

xai.py
@@ -13,7 +13,9 @@ def CAM(model, layer_act, layer_fc, x, y):
         model(x)
     h.remove()
     w = layer_fc.weight[y]
-    return torch.sum(w[..., None, None]*act, 1)
+    heatmap = torch.sum(w[..., None, None]*act, 1)
+    heatmap = heatmap / heatmap.amax((1, 2), True)
+    return heatmap
 
 # https://ieeexplore.ieee.org/document/8237336
 def GradCAM(model, layer_act, layer_fc, x, y):
@@ -33,7 +35,9 @@ def GradCAM(model, layer_act, layer_fc, x, y):
     bh.remove()
     # in the paper, they use relu to eliminate the negative values
     # (but maybe we want them to improve our metrics like degredation score)
-    return torch.sum(w[..., None, None]*act, 1)
+    heatmap = torch.sum(w[..., None, None]*act, 1)
+    heatmap = heatmap / heatmap.amax((1, 2), True)
+    return heatmap
 
 # https://arxiv.org/abs/1704.02685
 def DeepLIFT(model, layer_act, layer_fc, x, y):
@@ -43,7 +47,9 @@ def DeepLIFT(model, layer_act, layer_fc, x, y):
     pred_x = model(x)['class'][range(len(y)), y]
     delta = pred_x - pred_baseline
     delta.sum().backward()
-    return torch.mean((x - baseline) * x.grad, 1)
+    heatmap = torch.mean((x - baseline) * x.grad, 1)
+    heatmap = heatmap / heatmap.amax((1, 2), True)
+    return heatmap
 
 def Occlusion(model, layer_act, layer_fc, x, y):
     occ_w = x.shape[3]//7