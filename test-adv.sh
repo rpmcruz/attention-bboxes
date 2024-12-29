c02d126b2039125eb82f2a9b8c2c9b7ad4c84e0a
Ricardo Cruz <rpcruz@fe.up.pt>
2024-12-29 11:10:55 +0000

    Results.

--- test-adv.sh
+++ test-adv.sh
@@ -18,42 +18,42 @@ if [ $DATASET = StanfordDogs ]; then NCLASSES="10 50 80 120"; fi
 
 # baseline: protopnet
 MODEL="model-$DATASET-ProtoPNet.pth"
-python test.py $MODEL $DATASET --protopnet
-python test.py $MODEL $DATASET --protopnet --crop
+#python test.py $MODEL $DATASET --protopnet
+#python test.py $MODEL $DATASET --protopnet --crop
 # baseline: protopnet crop
 MODEL="model-$DATASET-ProtoPNet-crop.pth"
-python test.py $MODEL $DATASET --protopnet
-python test.py $MODEL $DATASET --protopnet --crop
+#python test.py $MODEL $DATASET --protopnet
+#python test.py $MODEL $DATASET --protopnet --crop
 
 for NCLASS in $NCLASSES; do
 # baseline: no heatmap (only class)
 MODEL="model-$DATASET$NCLASS-OnlyClass.pth"
-python test.py $MODEL $DATASET$NCLASS
+#python test.py $MODEL $DATASET$NCLASS
 
 # baseline: xai
 for xai in $XAI; do
-python test.py $MODEL "$DATASET$NCLASS" --xai $xai
+  : #python test.py $MODEL "$DATASET$NCLASS" --xai $xai
 done
 
 # baseline: ViT
 MODEL="model-$DATASET$NCLASS-ViTb.pth"
-python test.py $MODEL "$DATASET$NCLASS"
+#python test.py $MODEL "$DATASET$NCLASS"
 MODEL="model-$DATASET$NCLASS-ViTl.pth"
-python test.py $MODEL "$DATASET$NCLASS"
+#python test.py $MODEL "$DATASET$NCLASS"
 MODEL="model-$DATASET$NCLASS-ViTr.pth"
-python test.py $MODEL "$DATASET$NCLASS"
+#python test.py $MODEL "$DATASET$NCLASS"
 
 # proposal (with ablation)
 for occlusion in $OCCLUSIONS; do
     for penalty in $PENALTIES; do
         MODEL="model-$DATASET$NCLASS-Heatmap-l1-$penalty-heatmap-none-occlusion-$occlusion-sigmoid-1.pth"
-        python test.py $MODEL "$DATASET$NCLASS"
+        #python test.py $MODEL "$DATASET$NCLASS"
         model="model-$DATASET$NCLASS-Heatmap-l1-$penalty-heatmap-none-occlusion-$occlusion-sigmoid-1-adversarial-1.pth"
         python test.py $MODEL "$DATASET$NCLASS"
         for objdet in $MODELS; do
             for heatmap in $HEATMAPS; do
                 MODEL="model-$DATASET$NCLASS-$objdet-l1-$penalty-heatmap-$heatmap-occlusion-$occlusion-sigmoid-1.pth"
-                python test.py $MODEL "$DATASET$NCLASS"
+                #python test.py $MODEL "$DATASET$NCLASS"
                 MODEL="model-$DATASET$NCLASS-$objdet-l1-$penalty-heatmap-$heatmap-occlusion-$occlusion-sigmoid-1-adversarial-1.pth"
                 python test.py $MODEL "$DATASET$NCLASS"
             done
